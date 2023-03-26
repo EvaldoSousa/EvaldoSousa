314b6249566b805dc57ad47a2a8f7f81fe420b71
Evaldo Sousa <52108812+EvaldoSousa@users.noreply.github.com>
2023-03-26 18:58:49 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Analista de Software", "Analista de Suporte", "Pessimo jogador de Counter Strike"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 100,
+    loop: true
+
+})
